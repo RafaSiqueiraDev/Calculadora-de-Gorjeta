1abc08dd44fe6686d31fe0e5724bd6b317118cbc
Rafael Siqueira <FAELPSIQUEIRA@GMAIL.COM>
2024-07-21 22:51:29 +0100

    Projeto - Calculadora de Gorgeta
    Mostrando resultados em tela

---
+++ app/src/main/java/com/example/calculadoradegorjeta/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.calculadoradegorjeta
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.calculadoradegorjeta.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        var percentage: Int = 0
+
+        binding.rbOptionone.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                 percentage = 10
+            }
+        }
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                percentage = 20
+
+            }
+        }
+
+        binding.calcular.setOnClickListener{
+
+            val totalTableTemp = binding.tieTotal.text
+            val nPeopleTemp = binding.tieQtdPessoas.text
+
+            if(totalTableTemp?.isEmpty() ==true ||
+                nPeopleTemp?.isEmpty() == true) {
+
+                Snackbar.make(binding.tieTotal,"Preencha todos os campos",Snackbar.LENGTH_LONG)
+                    .show()
+
+            }else{
+                val totalTable: Float = totalTableTemp.toString().toFloat()
+                val nPeople: Float = nPeopleTemp.toString().toFloat()
+
+                val totalTemp = totalTable / nPeople
+                val tips = totalTemp * percentage /100
+                val totalWithTips = totalTemp + tips
+
+            }
+
+            binding.Limpar.setOnClickListener()
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
